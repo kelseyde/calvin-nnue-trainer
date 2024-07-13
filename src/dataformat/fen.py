b955ed3f4414afd4128e69d5b247c26e3d950e5b
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-07-13 10:58:56 +0200

    Init feature tensor as 1-dimensional without flattening

--- src/dataformat/fen.py
+++ src/dataformat/fen.py
@@ -6,13 +6,16 @@ CHAR_TO_PIECE_MAP = {
     'P': 0, 'N': 1, 'B': 2, 'R': 3, 'Q': 4, 'K': 5,
 }
 
+COLOUR_STRIDE = 64 * 6
+PIECE_STRIDE = 64
+
 
 def fen_to_features(fen):
     fen_parts = fen.split()
     piece_placement, active_color = fen_parts[:2]
     stm = 1 if active_color == 'w' else 0
-    white_features = np.zeros((2, 6, 64))
-    black_features = np.zeros((2, 6, 64))
+    white_features = np.zeros(768)
+    black_features = np.zeros(768)
 
     rank, file = 7, 0
     for char in piece_placement:
@@ -21,15 +24,15 @@ def fen_to_features(fen):
         elif char.isdigit():
             file += int(char)
         else:
-            piece_index = CHAR_TO_PIECE_MAP[char]
-            square_index = 8 * rank + file
+            piece_idx = CHAR_TO_PIECE_MAP[char]
+            square_idx = 8 * rank + file
             is_white = char.isupper()
-            white_features = update_features(white_features, square_index, piece_index, is_white, True)
-            black_features = update_features(black_features, square_index, piece_index, is_white, False)
+            white_features = update_features(white_features, square_idx, piece_idx, is_white, True)
+            black_features = update_features(black_features, square_idx, piece_idx, is_white, False)
             file += 1
 
-    white_features = torch.tensor(white_features.flatten(), dtype=torch.float32)
-    black_features = torch.tensor(black_features.flatten(), dtype=torch.float32)
+    white_features = torch.tensor(white_features, dtype=torch.float32)
+    black_features = torch.tensor(black_features, dtype=torch.float32)
     stm = torch.tensor([stm], dtype=torch.int8)
     stm_features = white_features if stm == 1 else black_features
     nstm_features = black_features if stm == 1 else white_features
@@ -37,9 +40,10 @@ def fen_to_features(fen):
     return stm_features, nstm_features, stm
 
 
-def update_features(features, square_index, piece_index, is_white_piece, is_white_perspective):
-    colour_index = 0 if is_white_piece == is_white_perspective else 1
+def update_features(features, square_idx, piece_idx, is_white_piece, is_white_perspective):
+    colour_idx = 0 if is_white_piece == is_white_perspective else 1
     if not is_white_perspective:
-        square_index ^= 56
-    features[colour_index][piece_index][square_index] = 1
+        square_idx ^= 56
+    feature_idx = colour_idx * COLOUR_STRIDE + piece_idx * PIECE_STRIDE + square_idx
+    features[feature_idx] = 1
     return features
